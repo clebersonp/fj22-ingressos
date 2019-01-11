e66215c1a8704033864f16e2145f8aa13478fac1
Cleberson <pauluci.cleberson@gmail.com>
2019-01-10 23:00:26 -0200

    Implementado descontos para compra de ingressos utilizando design patter strategy

---
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -0,0 +1,57 @@
+package br.com.caelum.ingresso.model.descontos;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import junit.framework.Assert;
+
+public class DescontoTest {
+
+	private Sessao sessao;
+
+	@Before
+	public void init() {
+		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
+		sessao = new Sessao(LocalTime.parse("10:00:00"), sala, filme);
+	}
+	
+	@Test
+	public void naoDeveConcederDescontoParaIngressoNormal() {
+		
+		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
+		
+		BigDecimal precoEsperado = new BigDecimal("32.50");
+		
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+	}
+	
+	@Test
+	public void deveConcederDescontoDe30PorcentoParaIngressosDeClientesDeBancos() {
+		
+		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		
+		BigDecimal precoEsperado = new BigDecimal("22.75");
+		
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+	}
+	
+	@Test
+	public void deveConcederDescontoDe50PorcentoParaIngressosDeEstudante() {
+		
+		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		
+		BigDecimal precoEsperado = new BigDecimal("16.25");
+		
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+	}
+
+}
